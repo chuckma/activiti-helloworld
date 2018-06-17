d5d5adf1f8b0a1d6fae5dbb454818b02417703d7
lucas ma <robbincen@163.com>
2018-06-17 22:13:53 +0800

    init activiti

---
+++ src/main/java/cn/lucasma/activiti/helloworld/DemoMain.java
@@ -0,0 +1,24 @@
+package cn.lucasma.activiti.helloworld;
+
+
+import java.util.logging.Logger;
+
+/**
+ * @Author Lucas Ma
+ * @Date 2018/6/17 下午9:35
+ *
+ * 启动类
+ */
+public class DemoMain {
+
+    public static void main(String[] args) {
+        System.out.println("启动程序");
+
+
+
+
+
+        System.out.println("结束程序");
+
+    }
+}
